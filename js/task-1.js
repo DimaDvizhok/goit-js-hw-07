c280be65c6c097553dd5dee52cc3aef502b16c95
Dvizhok <kalibry1@gmail.com>
2025-12-04 15:50:31 +0200

    1

--- js/task-1.js
+++ js/task-1.js
@@ -1,16 +1,16 @@
-function analyzeCategories() {
-  const categoriesElem = document.querySelectorAll('.item');
-  console.log(`Number of categories: ${categoriesElem.length}`);
+const categoriesElem = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categoriesElem.length}`);
 
+function foo() {
   categoriesElem.forEach(item => {
     const titleElem = item.querySelector('h2');
-    const categoryName = titleElem.textContent;
+    const name = titleElem.textContent;
 
     const ulElem = item.querySelector('ul');
     const items = ulElem.children.length;
 
-    console.log(`Kategory: ${categoryName}`);
+    console.log(`Kategory: ${name}`);
     console.log(`Elements: ${items}`);
   });
 }
-analyzeCategories();
+foo();
