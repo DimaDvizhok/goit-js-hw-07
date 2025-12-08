73deb21c7abf2a9c6797afd3c0e609552deb7833
Dvizhok <kalibry1@gmail.com>
2025-12-08 12:36:44 +0200

    1

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,15 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const colorSpanElem = document.querySelector('.color');
+const changeColorButtonElem = document.querySelector('.change-color');
+const bodyElem = document.body;
+
+changeColorButtonElem.addEventListener('click', changeColor);
+
+function changeColor() {
+  const newColor = getRandomHexColor();
+  bodyElem.style.backgroundColor = newColor;
+  colorSpanElem.textContent = newColor;
+}
